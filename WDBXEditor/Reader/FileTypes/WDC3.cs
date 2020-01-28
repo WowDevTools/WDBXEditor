13f4b49a956f8074c71bd4241d8a80c09e52cbc8
DintyGadget <windowstar24to7@gmail.com>
2020-01-29 05:01:18 +0900

    Create WDC3.cs

---
+++ WDBXEditor/Reader/FileTypes/WDC3.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WDBXEditor.Storage;
+using static WDBXEditor.Common.Constants;
+
+namespace WDBXEditor.Reader.FileTypes
+{
+    class WDC3 : WDC2
+    {
+
+    }
+}
